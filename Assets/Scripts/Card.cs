04b0f9a457d8f38d658cee24e023a0fa095ab077
Rem <ultimate.spidey.18@gmail.com>
2021-07-31 22:57:55 -0500

    Card Flip Routine

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,64 +4,69 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    [SerializeField] private bool m_isFaceUp = false;
+    [SerializeField] private bool m_isFaceUp = true;
 
     [SerializeField] private GameObject m_frontSide;
     [SerializeField] private GameObject m_backSide;
     [SerializeField] private SpriteRenderer[] m_suitSprites;
-    [SerializeField] private SpriteRenderer m_valueSprite;
+    [SerializeField] private SpriteRenderer[] m_valueSprites;
 
     private bool m_isLerping = false;
-    private float m_flipTime = 0.2f;
 
-    public void Flip()
+    public void Flip(float flipTime = 0.15f )
     {
-        StartCoroutine(FlipRoutine());
+        if (m_isLerping)
+            return;
+
+        StartCoroutine(FlipRoutine(flipTime));
     }
 
-    private void Update()
+    IEnumerator FlipRoutine(float flipTime = 0.15f)
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Flip();
-        }
-    }
+        if (flipTime <= 0.0f)
+            flipTime = 0.001f;
 
-    IEnumerator FlipRoutine()
-    {
         m_isLerping = true;
         float percent = 0f;
         float currentTime = 0f;
         bool hasSwitchedFaces = false;
 
-        Quaternion initRotation = transform.localRotation;
-        Quaternion desiredRotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y + 180, transform.eulerAngles.z);
+        Quaternion initRotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+        Quaternion endRotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y + 180, transform.eulerAngles.z);
 
         while (percent < 1.0f)
         {
-            transform.localRotation = Quaternion.Lerp(initRotation, desiredRotation, percent);
-
-            percent = currentTime / m_flipTime;
+            percent = currentTime / flipTime;
 
             currentTime += Time.deltaTime;
 
+            transform.rotation = Quaternion.Lerp(initRotation, endRotation, percent);
+
             if (percent >= 0.5f && !hasSwitchedFaces)
             {
+                m_isFaceUp = !m_isFaceUp;
+
                 m_frontSide.gameObject.SetActive(m_isFaceUp);
+
                 m_backSide.gameObject.SetActive(!m_isFaceUp);
+
                 hasSwitchedFaces = true;
             }
 
             yield return null;
         }
 
-        transform.localRotation = desiredRotation;
-
-        m_isFaceUp = !m_isFaceUp;
+        transform.rotation = endRotation;
 
         m_isLerping = false;
+    }
 
-
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Flip(0.15f);
+        }
     }
 
 }
