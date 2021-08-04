8bca8d8251e49ffb20d10d86854e786d24fd85b2
Rem <ultimate.spidey.18@gmail.com>
2021-08-04 14:21:52 -0500

    Scripts Cleanup
    Deleted unused scripts

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private int m_score = 0;
+    private Dictionary<Card, bool> m_cardsCountedTowardsScoreFromGamePiles;
+    private Dictionary<Card, bool> m_cardsCountedTowardsScoreFromEndPiles;
+
+    private void Awake()
+    {
+        InitScore();
+    }
+
+    void InitScore()
+    {
+        m_score = 0;
+        m_cardsCountedTowardsScoreFromGamePiles = new Dictionary<Card, bool>();
+        m_cardsCountedTowardsScoreFromEndPiles = new Dictionary<Card, bool>();
+    }
+}
